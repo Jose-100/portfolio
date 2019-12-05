66e64e4ff3ace6bd42f4491c96c149dabc0a3760
Jose Redondo <jredondo@pernixlabs.com>
2019-12-05 13:15:58 -0600

    Post status has been updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show' , as: 'portfolio_show'
-  resources :categories
-  resources :blogs
-  # get "pages/home"
-  get 'about', to: 'pages#about' 
-  get 'contact', to: 'pages#contact' 
+   # get "pages/home"
+   get 'about', to: 'pages#about' 
+   get 'contact', to: 'pages#contact' 
+ 
+    resources :categories
   
+    resources :blogs do
+    member do
+      get :toggle_status 
+    end 
+  end 
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
